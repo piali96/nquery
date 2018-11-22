f495f5868b758e51ddf1e87876e10f1112cb583c
Piali Priyadarshini <piali96@gmail.com>
2018-11-23 03:32:27 +0530

    Pusher stuff deleted

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,6 +1,7 @@
 var authController = require('../controllers/auth.controller'),
     express = require('express'),
     passportService = require('../../config/auth');
+    passport = require('passport');
 
 var requireAuth = passport.authenticate('jwt',{session: false});
 var requireLogin = passport.authenticate('local', {session:false});
