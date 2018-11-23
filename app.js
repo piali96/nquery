6736e722dccf63f3926de85bedcd6b342f204c20
Piali Priyadarshini <piali96@gmail.com>
2018-11-23 15:21:52 +0530

    hopefully chatbot works now or else I will cry

--- app.js
+++ app.js
@@ -14,22 +14,57 @@ var databaseConfig = require('./config/database');
 
  
 mongoose.connect(databaseConfig.url,{ useNewUrlParser: true });
- 
+
+//Chatbot stuff
+const Pusher = require('pusher')
+require('dotenv').config()
+const shortId = require('shortid') 
+const dialogFlow = require('./dialogflow')
+
+
 
 app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({ extended: true })); // Parses urlencoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 app.use(logger('dev')); // Log requests to API using morgan
 app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_APP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+    cluster: 'ap2',
+    encrypted: true
+  })
 
 //  Adding all .routes.js files 
 require('./app/routes/auth.routes.js')(app);
 require('./app/routes/questions.routes.js')(app);
 
+app.post('/message', async (req, res) => {
+    // simulate actual db save with id and createdAt added
+    console.log(req.body);
+    const chat = {
+      ...req.body,
+      id: shortId.generate(),
+      createdAt: new Date().toISOString()
+    } 
+    //update pusher listeners
+    pusher.trigger('chat-bot', 'chat', chat)
 
+    const message = chat.message;
+    const response = await dialogFlow.send(message);
+    // trigger this update to our pushers listeners
+    pusher.trigger('chat-bot', 'chat', {
+      message: `${response.data.result.fulfillment.speech}`,
+      type : 'bot',
+      createdAt : new Date().toISOString(),
+      id: shortId.generate()
+    })
+    res.send(chat)
+  })
 
 app.listen(port = process.env.PORT || 8000,()=>{
     console.log(`Listening on port ${port}`);
