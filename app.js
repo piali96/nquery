147efb86136ec6b06999a345303090cf7eeff513
Piali Priyadarshini <piali96@gmail.com>
2018-11-23 13:10:14 +0530

    added chatbot stuff

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var mongoose = require('mongoose');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-const Pusher = require('pusher')
-const shortId = require('shortid') 
-const dialogFlow = require('./dialogflow')
 
 // var config = require('./config/config')
 var passport = require('passport');
@@ -27,36 +24,6 @@ app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Chatbot Stuff
-const pusher = new Pusher({
-    appId: process.env.PUSHER_APP_ID,
-    key: process.env.PUSHER_APP_KEY,
-    secret: process.env.PUSHER_APP_SECRET,
-    cluster: 'ap2',
-    encrypted: true
-  })
-  app.post('/message', async (req, res) => {
-    // simulate actual db save with id and createdAt added
-    console.log(req.body);
-    const chat = {
-      ...req.body,
-      id: shortId.generate(),
-      createdAt: new Date().toISOString()
-    } 
-    //update pusher listeners
-    pusher.trigger('chat-bot', 'chat', chat)
-
-    const message = chat.message;
-    const response = await dialogFlow.send(message);
-    // trigger this update to our pushers listeners
-    pusher.trigger('chat-bot', 'chat', {
-      message: `${response.data.result.fulfillment.speech}`,
-      type : 'bot',
-      createdAt : new Date().toISOString(),
-      id: shortId.generate()
-    })
-    res.send(chat)
-  })
 
 //  Adding all .routes.js files 
 require('./app/routes/auth.routes.js')(app);
